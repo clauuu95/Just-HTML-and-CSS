c4c49c240b842fcc7a791230d6377f6707ff3dc2
clauuu95 <claudiacoyo@gmail.com>
2017-08-25 08:17:10 -0300

    Add files via upload

---
+++ HTML5Application/public_html/newjavascript.js
@@ -0,0 +1,8 @@
+function focus(){
+    var facebook = document.getElementById("face");
+    facebook.style.display = "flex";
+}
+function leave(){
+    var facebook = document.getElementById("face");
+    facebook.style.display = "flex";
+}
